9b44b3ddab23fe711d0f9a57f4022d90c76f7c50
Michael Wiciak <86964604+MichaelWiciak@users.noreply.github.com>
2021-07-05 22:22:12 +0100

    Add files via upload

---
+++ Merge Sort ~ w time indicator.py
@@ -0,0 +1,39 @@
+import time
+
+def merge_sort(m):
+	if len(m) <= 1:
+		return m
+ 
+	middle = len(m) // 2
+	left = m[:middle]
+	right = m[middle:]
+ 
+	left = merge_sort(left)
+	right = merge_sort(right)
+	return list(merge(left, right))
+
+def merge(left, right):
+	result = []
+	left_idx, right_idx = 0, 0
+	while left_idx < len(left) and right_idx < len(right):
+		if left[left_idx] <= right[right_idx]:
+			result.append(left[left_idx])
+			left_idx += 1
+		else:
+			result.append(right[right_idx])
+			right_idx += 1
+	if right_idx == len(right):
+		result.extend(left[left_idx:])
+	else:
+		result.extend(right[right_idx:])
+	return result
+
+someList = []
+for i in range(100):
+	someList.append(str(i))
+print(someList)
+start = time.time()
+someList = merge_sort(someList)
+end = time.time()
+print(end - start, 'seconds')
+print(someList)
